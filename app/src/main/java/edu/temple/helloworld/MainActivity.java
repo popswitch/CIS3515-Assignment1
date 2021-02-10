e3261bef16cc1fd4221929240fe999737e4a3026
Erika Garcia <tug00581@temple.edu>
2021-02-10 00:01:54 -0500

    Added button event to change text

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -1,14 +1,32 @@
 package edu.temple.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
+import android.widget.TextView;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        View.OnClickListener onClickListener = new ButtonClickListener();
+        button.setOnClickListener(onClickListener);
+    }
+
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            textView.setText("You clicked this button.");
+        }
     }
 }
